6a677a95c496356e89d3a96888d11e4f5aef0c41
Luke Atkinson-Coyle <lukeatkinson-coyle@Lukes-MacBook-Pro.local>
2025-08-04 12:11:01 +0100

    remove shopping cart

--- src/index.tsx
+++ src/index.tsx
@@ -4,12 +4,11 @@ import "./index.css";
 // import App from "./Likes_Button/App";
 // import App from "./useHoverHook/App";
 // import App from "./useFocusHook/App";
-import App from "./ShoppingCart/App";
 // import App from "./Noughts_Crosses/App";
 // import App from "./Data_Fetching/App";
 // import App from "./Todo/App";
 // import App from "./Form/App";
-// import App from "./Overlay/App";
+import App from "./Overlay/App";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
